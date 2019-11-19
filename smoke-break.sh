39ae147b1eb1af0b6ff5c88715410dbe3bccdf0a
Stan Verschuuren <sverschu@f0r6s10.codam.nl>
2019-11-19 20:37:09 +0100

    13

--- smoke-break.sh
+++ smoke-break.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 NAME="run-test"
+MAX=1000
 if [ ! -f "./$NAME" ]; then make love && clear; fi 
 echo go for a smokebreak while randomized tests will try to fuck with your code
 echo hit ctrl+c to kill the test
 echo smoking in.. && echo 3 && sleep 1 && echo 2 && sleep 1 && echo 1 && sleep 1
 clear
-i=0; while (./run-test); do i=$((i+1)); printf "$i" sleep 0.1; done
+i=0; while (./run-test); do i=$((i+1)); printf "$i"; if [ $i -gt $MAX ]; then break; fi; sleep 0.1; done
